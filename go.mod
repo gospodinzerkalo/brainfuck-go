2940e420689f514660b635b7c2436a45dd5e023d
gospodinzerkalo <darhan020818@gmail.com>
2021-11-17 11:17:16 +0600

    parse simple loop

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/gospodinzerkalo/brainfuck-go
+
+go 1.16
+
+require github.com/urfave/cli/v2 v2.3.0
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"github.com/urfave/cli/v2"
+	"os"
+)
+
+var (
+	input = ""
+
+	flags = []cli.Flag{
+		&cli.StringFlag{
+			Name:        "input",
+			Aliases:     []string{"i"},
+			Usage: 		"enter input in terminal",
+			Destination: &input,
+		},
+	}
+
+)
+
+func main(){
+	app := cli.NewApp()
+	app.Commands = cli.Commands{
+		&cli.Command{
+			Name: "run",
+			Action: start,
+			Flags: flags,
+			Usage: "run script",
+		},
+	}
+	app.Run(os.Args)
+}
+
+func start(c *cli.Context) error {
+	pos := 0
+	mapPos := 0
+	d := make(map[int]int64, 0)
+	for i := 0; i < len(input); i++ {
+		switch input[i] {
+		case '+':
+			if _, ok := d[pos]; ok {
+				d[pos] += 1
+			} else {
+				d[pos] = 1
+			}
+		case '-':
+			d[pos] -= 1
+		case '<':
+			pos -= 1
+		case '>':
+			pos += 1
+		case '[':
+			mapPos = i
+		case ']':
+			if d[pos] == 0 {
+				continue
+			} else {
+				i = mapPos
+			}
+		case ',':
+		case '.':
+			fmt.Print(string(d[pos]), d[pos])
+		}
+	}
+	return nil
+}
